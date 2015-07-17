312e9f56d1aa02ec2ffbc6dd055948bff7f1ffa0
Nikolaos Marmaridis Office <nmarmaridis@velti.com>
2015-07-17 14:13:08 +0300

    NavCtrl added

---
+++ app/scripts/controllers/nav.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elasticWebAppApp.controller:NavCtrl
+ * @description
+ * # NavCtrl
+ * Controller of the elasticWebAppApp
+ */
+angular.module('elasticWebAppApp')
+  .controller('NavCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
